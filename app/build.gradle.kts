4a5ae3a925fe3b099f82399b31cfd58ca6cfbcd3
Klaus Gjelstrup Nielsen <klaus@kgnielsen.dk>
2026-06-06 19:54:31 +0200

    Fix Android theme resources for successful debug build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
